10e5c888fb80f3397065bb753feef95b8c267d0f
skiee9 <chaudharyayushi785@gmail.com>
2025-02-03 21:31:52 +0530

    update the css

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,7 +3,13 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      
+      <h1>Stream movies online</h1>
+      <p>Need some help deciding what to watch before all your popcorn runs out? We’ve got you covered! With this guide you can find the best movies to watch online across all languages, eras, and genres all under one roof. From seeing where to watch movies for free to keeping track of all the new movie releases, our easy-to-use guide is your go-to hub for all things movies.
+
+Where to watch movies online?
+Want to know where to watch your favourite movies online? With a growing catalogue of more than 96,000+ films across all major streaming platforms, our movie streaming guide is the ideal way to keep track of where to buy, rent or stream movies online. Our user-friendly search filters also make it easy to filter movies based on your personal preferences. For example, if you are interested in revisiting the golden era of Bollywood, you can click on the ‘Release Year’ filter and slide your cursor over to the film era you’d like to stream.
+
+Do you want recommendations that cater specifically to your interests? With JustWatch you can filter your results by genre and only see streaming options for romance, action, thriller, musicals, comedy, and fourteen other genres. Simply click on ‘genre’, select the genres that appeal to you and start browsing through the best movies to watch based on your interests.</p>
     </div>
   )
 }
--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,47 +1,56 @@
-import React, {useContext, useState} from 'react'
-import axios from 'axios'
-import { AuthContext } from '../context/AuthContextProvider'
-
+import React, { useState, useContext } from "react";
+import axios from "axios";
+import { AuthContext } from "../context/AuthContextProvider";
+import "../styles/Login.css";
 const Login = () => {
-    const [username,setUsername]= useState("")
-    const [password,setPassword]= useState("")
-    const [error,setError] = useState(null)
-    // const [token, setToken]= useState(null)
-    const {login}= useContext(AuthContext)
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
+	const [error, setError] = useState(null);
+	const { login } = useContext(AuthContext); 
 
-    const handleLogin =async (e)=>{
-        e.preventDefault()
-        try{
-           const response = await axios({
-            method: 'POST',
-            url:'https://achieved-colossal-skink.glitch.me/login',
-            data:{
-              username,password
-            }
-           })
-           if(response.data.success){
-            console.log(response.data.token,"response")
-            const {token}=response.data
-            Login(token)
-            // setToken(response.data.token)
-           } 
-           }catch(err){
-            // setError(err)
-            setError(err.response.data.token,"response")
+	const handleLogin = async (e) => {
+		e.preventDefault();
+		try {
+			const response = await axios.post(
+				`https://achieved-colossal-skink.glitch.me/login`,
+				{
+					username,
+					password,
+				},
+			);
+			console.log(response);
+			if (response.data.success) {
+				console.log(response.data.token, "response");
+				const { token } = response.data;
+				login(token); 
+			}
+		} catch (err) {
+			console.log(err);
+		
+		}
+	};
 
-        }
-    }
+	return (
+		<div className="login-container">
+			<h1>Login</h1>
+			<form onSubmit={handleLogin}>
+				<input
+					type="text"
+					placeholder="Enter username"
+					value={username}
+					onChange={(e) => setUsername(e.target.value)}
+				/>
+				<input
+					type="password"
+					placeholder="Enter Password"
+					value={password}
+					onChange={(e) => setPassword(e.target.value)}
+				/>
+				<input type="submit" value="Login" />
+			</form>
+			{error && <p style={{ color: "red" }}>{error}</p>}
+		</div>
+	);
+};
 
-
-  return (
-   <>
-<form onSubmit= {handleLogin}>
-    <input type="text" placeholder="enter your username" value={username} onChange={(e)=>setUsername(e.target.value)}/>
-    <input type="password" placeholder="enter your password" value={password} onChange={(e)=>setPassword(e.target.value)}/>
-    <input type="submit" value="Login"/>
-</form>
-   </>
-  )
-}
-
-export default Login
+export default Login;
